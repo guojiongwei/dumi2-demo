46c781ddd044e0d5abb3840416f76687ed7b7c53
guojiongwei <g17637907938@163.com>
2023-02-12 23:28:38 +0800

    feat: 避免打包demo文件到npm包里面

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  esm: {
+    output: 'dist',
+    ignores: [
+      'src/**/demo/**', // 避免打包demo文件到npm包里面
+    ],
+  },
 });
