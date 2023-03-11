694ab49ee668bb13373ff03dc7a66690c66d4f22
guojiongwei <g17637907938@163.com>
2023-03-11 23:39:51 +0800

    feat: 调整左侧菜单列表样式

--- .dumirc.ts
+++ .dumirc.ts
@@ -7,4 +7,17 @@ export default defineConfig({
   },
   // @ts-ignore
   exportStatic: false,
+  styles: [
+    `.dumi-default-features-item {
+      text-align: center;
+    }
+    body .dumi-default-sidebar>dl>dt {
+      font-size: 16px;
+    }
+    body .dumi-default-sidebar>dl>dd>a {
+      color: #717484;
+      font-size: 14px;
+    }
+    `,
+  ],
 });
