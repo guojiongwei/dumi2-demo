727ba53f92d0ab67eb7a35327bd8d58a4753e6c6
guojiongwei <g17637907938@163.com>
2023-03-21 20:31:42 +0800

    feat: 添加button按钮单元测试

---
+++ src/Button/__tests__/index.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button from '../index';
+
+describe('Button组件', () => {
+  it('能够正确渲染按钮文字', () => {
+    const buttonText = '按钮文字';
+    const { getByRole } = render(<Button>{buttonText}</Button>);
+    const buttonElement = getByRole('button');
+    expect(buttonElement.innerHTML).toBe(buttonText);
+  });
+
+  it('能够正确渲染默认样式的按钮', () => {
+    const { getByRole } = render(<Button >默认按钮</Button>);
+    const buttonElement = getByRole('button');
+    expect(buttonElement.classList.contains('button')).toBe(true);
+  });
+
+  it('能够正确渲染主要样式的按钮', () => {
+    const { getByRole } = render(<Button type="primary">主要按钮</Button>);
+    const buttonElement = getByRole('button');
+    expect(buttonElement.classList.contains('button-primary')).toBe(true);
+  });
+
+  it('能够触发点击事件', () => {
+    const handleClick = jest.fn();
+    const { getByRole } = render(<Button type="primary">点击按钮</Button>);
+    const buttonElement = getByRole('button');
+    fireEvent.click(buttonElement);
+    expect(handleClick).toHaveBeenCalledTimes(0);
+  });
+});
