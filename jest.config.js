7070ee12b067f92892c6fa32ee18d48e69f89b92
guojiongwei <g17637907938@163.com>
2023-03-30 17:22:49 +0800

    feat: 配置完善jest配置

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   roots: ['./src'], // 查找src目录中的文件
   collectCoverage: true, // 统计覆盖率
   coverageDirectory: 'coverage', // 覆盖率结果输出的文件夹
+  transform: {
+		'\\.(less|css)$': 'jest-less-loader' // 支持less
+	},
   // 单元覆盖率统计的文件
   collectCoverageFrom: ['src/**/*.tsx', '!src/index.ts', '!src/**/demo/*'],
 };
