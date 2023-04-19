b34dd3f6122ad79e0f048fc6771b91f16bb22123
guojiongwei <g17637907938@163.com>
2023-04-19 23:04:27 +0800

    feat: 删除Foo组件

--- src/Foo/index.md
+++
@@ -1,9 +0,0 @@
-# Foo
-
-This is an example component.
-
-```jsx
-import { Foo } from 'dumi2-demo';
-
-export default () => <Foo title="Hello dumi!" />;
-```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as Button } from './Button';
-export { default as Foo } from './Foo';
 export { default as formatDate } from './formatDate';
 export { default as PrimaryButton } from './PrimaryButton';
